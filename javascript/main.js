71c02dda95da4915f2efeb378f720dfd9831e16b
Amairani Arellano <lyliarellano@gmail.com>
2025-04-28 17:02:41 -0600

    responsividad del backround, nav y proyectos

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,32 @@
+// Carrusel
 const slider = document.querySelector('.slider-inner');
 const images = document.querySelectorAll('.slider-inner img');
 const totalImages = images.length;
-
 let index = 0;
 
-// Función para actualizar el carrusel
 function updateSlider() {
   slider.style.transform = `translateX(-${index * 500}px)`;
 }
 
-// Botón para avanzar
 document.getElementById('next-btn').addEventListener('click', () => {
-  index = (index + 1) % totalImages; // Reinicia al llegar al final
+  index = (index + 1) % totalImages;
   updateSlider();
 });
 
-// Botón para retroceder
 document.getElementById('prev-btn').addEventListener('click', () => {
-  index = (index - 1 + totalImages) % totalImages; // Ir al último al retroceder desde el primero
+  index = (index - 1 + totalImages) % totalImages;
   updateSlider();
 });
 
-// Desplazamiento automático
 setInterval(() => {
   index = (index + 1) % totalImages;
   updateSlider();
-}, 5000); // Pausa de 5 segundos
+}, 5000);
 
-//Carrusel
+// Menú Hamburguesa
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('nav-open');
+});
